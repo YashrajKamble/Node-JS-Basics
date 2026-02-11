0fd5a71b49b3dc10eb34c7e4f7be3ff7f36ba23e
Yashraj_K <yashrajkamble7727@gmail.com>
2026-02-11 19:15:53 +0530

    ES Modules

--- modulefirst.js
+++ modulefirst.js
@@ -1,3 +1,6 @@
-const simple = require("./modulesecond")
+// import { simple2 as simple } from "./modulesecond.mjs"
+import * as a2 from "./modulesecond.mjs"
+// const simple = require("./modulesecond.mjs")
 
-simple()
+// simple()
+console.log(a2.simple())
--- modulesecond.js
+++
@@ -1,5 +0,0 @@
-function simple() {
-    console.log("simple is complex")
-}
-
-module.exports = simple;
