6b1ae3bec906a8bb8c56815a37b05c3a21dbb57a
Yashraj_K <yashrajkamble7727@gmail.com>
2026-02-11 17:54:46 +0530

    in build functions

--- second.js
+++ second.js
@@ -1,7 +1,10 @@
+// (function (exports, require, module, __filename, __dirname) {
+
 yash = {
     name: "yashraj",
     favNum: 45,
     developer: true
 }
-
+console.log(exports, "  exports1", require, "  require2", module, "  module3", __filename, "  __filename4", __dirname, "  __dirname5")
 module.exports = yash;
+// })
